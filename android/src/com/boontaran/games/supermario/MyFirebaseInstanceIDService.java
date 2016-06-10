2d380b0813ab3d5d9331d3a9b5d35963711193ed
Jeongsang Baek <jsbaek@igaworks.com>
2016-06-10 17:37:33 +0900

    add google-services to integrate firebase SDK

---
+++ android/src/com/boontaran/games/supermario/MyFirebaseInstanceIDService.java
@@ -0,0 +1,7 @@
+package com.boontaran.games.supermario;
+
+/**
+ * Created by Jason on 2016-06-10.
+ */
+public class MyFirebaseInstanceIDService {
+}
---
+++ android/src/com/boontaran/games/supermario/MyFirebaseMessagingService.java
@@ -0,0 +1,9 @@
+package com.boontaran.games.supermario;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by Jason on 2016-06-10.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
